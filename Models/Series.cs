d9462e415b9c3570569631b90164a36292755f5a
saya <saya@DESKTOP-HBMAOK5>
2026-03-29 04:58:19 +0900

    作品名登録追加

---
+++ Models/Series.cs
@@ -0,0 +1,17 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyGoodsApp.Models
+{
+    [Table("series")]
+    public class Series : BaseModel
+    {
+        [PrimaryKey("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+    }
+}
