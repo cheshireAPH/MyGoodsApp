fbd04234d020b648bd73595cf5a56c751c5f5d76
saya <saya@DESKTOP-HBMAOK5>
2026-03-29 05:18:38 +0900

    キャラ一覧 追加

---
+++ Models/Character.cs
@@ -0,0 +1,17 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace MyGoodsApp.Models;
+
+[Table("characters")]
+public class Character : BaseModel
+{
+    [PrimaryKey("id", false)]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [Column("series_id")]
+    public int SeriesId { get; set; }
+}
