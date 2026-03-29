f9bf1c61d9323b96a1db0e96e156ba3deebbcb15
saya <saya@DESKTOP-HBMAOK5>
2026-03-29 16:27:03 +0900

    商品登録　更新

--- Models/ProductVariantCharacter.cs
+++ Models/ProductVariantCharacter.cs
@@ -1,15 +1,14 @@
 ﻿using Supabase.Postgrest.Attributes;
 using Supabase.Postgrest.Models;
 
-namespace MyGoodsApp.Models
+[Table("product_variant_characters")]
+public class ProductVariantCharacter : BaseModel
 {
-    [Table("product_variant_characters")]
-    public class ProductVariantCharacter : BaseModel
-    {
-        [Column("product_variant_id")]
-        public Guid ProductVariantId { get; set; }
+    [PrimaryKey("product_variant_id")]
+    [Column("product_variant_id")]
+    public Guid ProductVariantId { get; set; }
 
-        [Column("character_id")]
-        public int CharacterId { get; set; }
-    }
+    [PrimaryKey("character_id")]
+    [Column("character_id")]
+    public int CharacterId { get; set; }
 }
