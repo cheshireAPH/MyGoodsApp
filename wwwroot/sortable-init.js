a1c5c9a10f3d47d93c7a1f5bcbb306d691e50548
saya <saya@DESKTOP-HBMAOK5>
2026-04-02 06:23:39 +0900

    登録画面　更新

---
+++ wwwroot/sortable-init.js
@@ -0,0 +1,16 @@
+﻿window.initSortable = (elementId, dotnetRef, listName) => {
+    const el = document.getElementById(elementId);
+    if (!el) return;
+
+    Sortable.create(el, {
+        animation: 150,
+        handle: ".drag-handle",
+        direction: "vertical",
+        ghostClass: "sortable-ghost",
+        fallbackOnBody: true,
+        swapThreshold: 0.65,
+        onEnd: function (evt) {
+            dotnetRef.invokeMethodAsync("OnReordered", listName, evt.oldIndex, evt.newIndex);
+        }
+    });
+};
