103d0c346e9dbc6d5235fcc17df7c166f48bfd71
saya <saya@DESKTOP-HBMAOK5>
2026-09-23 20:43:36 +0900

    作品一覧　更新

--- wwwroot/js/longpress.js
+++ wwwroot/js/longpress.js
@@ -5,7 +5,23 @@
         element.addEventListener("pointerdown", () => {
             timer = setTimeout(() => {
                 dotnetHelper.invokeMethodAsync("OnLongPress", id);
-            }, 600); // 長押し判定
+            }, 600);
+        });
+
+        element.addEventListener("pointerup", () => clearTimeout(timer));
+        element.addEventListener("pointerleave", () => clearTimeout(timer));
+    },
+
+    registerById: function (id, dotnetHelper, seriesId) {
+        const element = document.getElementById(id);
+        if (!element) return;
+
+        let timer;
+
+        element.addEventListener("pointerdown", () => {
+            timer = setTimeout(() => {
+                dotnetHelper.invokeMethodAsync("OnLongPress", seriesId);
+            }, 600);
         });
 
         element.addEventListener("pointerup", () => clearTimeout(timer));
